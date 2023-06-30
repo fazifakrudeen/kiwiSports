a8edca69cf394c5a49447beaa79fd158c6aef873
Mohammed Fazil <fakrudeenfazil@gmail.com>
2023-06-30 20:14:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Location from "./components/Location";
+import Newletter from "./components/Newletter";
+import Cards from "./components/Cards";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="scroll-smooth hover:scroll-auto">
+      <Navbar />
+      <Hero />
+      <Location/>
+      <Newletter />
+      <Cards />
     </div>
+    
   );
 }
 
